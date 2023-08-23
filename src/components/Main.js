1c74e332f674a53425537cc2cbec0d3aa411604d
Betawi1 <Rashdan.ibrahim@gmail.com>
2023-08-23 11:40:24 +0800

    first commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -26,14 +26,19 @@ class Main extends Component {
           <br></br>
           <h1 className="text-white text-center">DigitalSelf Token ("DSF") <br/>Private Swap</h1>
           <br></br>
-          <br></br>
+          
         </div>
         <div className="card mb-4" >
           <div className="card-body">
             {content}
           </div>
         </div>
-        
+        <br/>
+        <div>
+        <h5 className="text-white text-center">HELP? This Video will show you how to Install MetaMask on mobile devices PCs , and how to purchase DSF Tokens</h5>
+        <br/>
+        <iframe align="center" width="565" height="360" src="https://www.youtube.com/embed/KCz7UAPicGg" frameborder="0" allowfullscreen></iframe>
+        </div>
       </div>
     );
   }
